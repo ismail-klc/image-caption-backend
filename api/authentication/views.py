fc083a5434d78749e4f8dc1be583c44713bf1171
ismail kılıç <ismail000728@gmail.com>
2021-04-25 19:15:48 +0300

    auth implemented

--- api/authentication/views.py
+++ api/authentication/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from .serializers import MyTokenObtainPairSerializer
+from rest_framework.permissions import AllowAny
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework import generics
 
-# Create your views here.
+from django.contrib.auth.models import User
+from .serializers import RegisterSerializer
+
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    permission_classes = (AllowAny,)
+    serializer_class = RegisterSerializer
+
+
+class MyObtainTokenPairView(TokenObtainPairView):
+    permission_classes = (AllowAny,)
+    serializer_class = MyTokenObtainPairSerializer
