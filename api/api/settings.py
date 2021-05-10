e6ca26a60fe8ab171fea72eca5c861d1ac540f2f
ismail kılıç <ismail000728@gmail.com>
2021-05-10 17:59:11 +0300

    db update

--- api/api/settings.py
+++ api/api/settings.py
@@ -104,7 +104,9 @@ WSGI_APPLICATION = 'api.wsgi.application'
 
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=600)
-DATABASES['default'].update(db_from_env)
+DATABASES = { 
+    'default': dj_database_url.config(conn_max_age=600) 
+    }
 
 
 # Password validation
