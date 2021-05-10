2eaacc3fd209da83580f080c024881ed82227f0b
ismail kılıç <ismail000728@gmail.com>
2021-05-10 18:05:44 +0300

    a

--- api/api/settings.py
+++ api/api/settings.py
@@ -144,8 +144,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-
+import os
 STATIC_URL = '/static/'
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
